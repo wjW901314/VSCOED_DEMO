f49c68af14c5f8f6fd8950e10f2588f09617be58
wjw901314 <492677104@qq.com>
2018-12-28 16:49:19 +0800

    项目搭建

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VSCOED_DEMO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
